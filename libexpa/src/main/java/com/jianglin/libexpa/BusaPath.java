4e481e76a714e40774dc11dc419611a61a635c37
jiang <jianglinair@163.com>
2021-02-02 10:45:35 +0800

    expa 1

---
+++ libexpa/src/main/java/com/jianglin/libexpa/BusaPath.java
@@ -0,0 +1,12 @@
+package com.jianglin.libexpa;
+
+/**
+ * Author: Jianglin
+ * Time: 2021/2/2
+ * Description: todo
+ *
+ * @author jiang
+ */
+public class BusaPath {
+    public static final String BUSA = "/a/busa";
+}
